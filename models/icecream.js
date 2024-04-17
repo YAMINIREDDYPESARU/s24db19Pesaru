dac30bf67d66a93c274d28f0989b4bee940e4382
YAMINIREDDYPESARU <156263074+YAMINIREDDYPESARU@users.noreply.github.com>
2024-04-17 16:21:16 -0500

    bonus

--- models/icecream.js
+++ models/icecream.js
@@ -1,7 +1,17 @@
 const mongoose = require("mongoose")
 const icecreamSchema = mongoose.Schema({
-icecream_name: String,
+icecream_name: {
+    type: String,
+    required: true,
+    minlength: 4,
+    maxlength: 50
+  },
 icecream_color: String,
-icecream_price: Number
+icecream_price: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 1000
+  }
 })
 module.exports = mongoose.model("icecream",icecreamSchema)
--- routes/icecream.js
+++ routes/icecream.js
@@ -1,17 +1,25 @@
 var express = require('express');
 const icecream_controller= require('../controllers/icecream');
+
 var router = express.Router();
+
+const secured = (req, res, next) => {
+    if (req.user) {
+      return next();
+    }
+    res.redirect("/login");
+  }
 /* GET icecream */
 router.get('/', icecream_controller.icecream_view_all_Page );
 /* GET detail icecream page */
 router.get('/detail', icecream_controller.icecream_view_one_Page);
 module.exports = router;
 /* GET create icecream page */
-router.get('/create', icecream_controller.icecream_create_Page);
+router.get('/create', secured,icecream_controller.icecream_create_Page);
 /* GET create update page */
-router.get('/update', icecream_controller.icecream_update_Page);
+router.get('/update', secured,icecream_controller.icecream_update_Page);
 
 /* GET delete icecream page */
-router.get('/delete', icecream_controller.icecream_delete_Page);
+router.get('/delete',secured, icecream_controller.icecream_delete_Page);
 
 module.exports = router
